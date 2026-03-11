188fa4f876cdff3cb2054154f3c920df6d533d0c
Xx-IKERO-90-xX <ikerjrh2003@gmail.com>
2026-03-11 13:21:02 +0100

    Intalamos tailwindcss y DaisyUI de manera local en lugar de usar CDN

--- SpringBoot/dam/vtinstitute/vtinstitute-server/src/main/java/com/vtinstitute/vtinstitute_restapi/config/WebConfig.java
+++ SpringBoot/dam/vtinstitute/vtinstitute-server/src/main/java/com/vtinstitute/vtinstitute_restapi/config/WebConfig.java
@@ -6,11 +6,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-            .addResourceHandler("/images/**")
-            .addResourceLocations("classpath:/static/images/");
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
     }
 }
--- SpringBoot/dam/vtinstitute/vtinstitute-server/src/main/java/com/vtinstitute/vtinstitute_restapi/model/dao/CoursDAO.java
+++ SpringBoot/dam/vtinstitute/vtinstitute-server/src/main/java/com/vtinstitute/vtinstitute_restapi/model/dao/CoursDAO.java
@@ -3,8 +3,13 @@ package com.vtinstitute.vtinstitute_restapi.model.dao;
 import org.springframework.data.repository.CrudRepository;
 import com.vtinstitute.vtinstitute_restapi.model.entity.Cours;
 import org.springframework.stereotype.Repository;
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
+
+
+import com.vtinstitute.vtinstitute_restapi.model.entity.Enrollment;
+import com.vtinstitute.vtinstitute_restapi.model.entity.Student;
+
 
 @Repository
-public interface CoursDAO extends CrudRepository<Cours, Integer> {
-
-}
+public interface CoursDAO extends CrudRepository<Cours, Integer> {}
